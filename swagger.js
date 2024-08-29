407293c1ff39c96ff317d743b9999290a437d0ef
xezernn <xezernovruz79@gmail.com>
2024-08-29 23:24:25 +0400

    swagger fix2

--- swagger.js
+++ swagger.js
@@ -1,20 +1,52 @@
 const swaggerJsdoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
 
+// const options = {
+//   definition: {
+//     openapi: '3.0.0',
+//     info: {
+//       title: 'Neptun Express Backend API',
+//       version: '1.0.1',
+//       description: 'Qısaca API-nin təsviri',
+//     },
+//     components: {
+//       securitySchemes: {
+//         bearerAuth: {
+//           type: 'http',
+//           scheme: 'bearer',
+//           bearerFormat: 'JWT', // Obyektiv format (bu JWT üçün nümunədir)
+//         },
+//       },
+//     },
+//     security: [
+//       {
+//         bearerAuth: [],
+//       },
+//     ],
+//   },
+//   apis: ['./src/routes/*.js'], // Path to your API routes
+// };
+
+
 const options = {
   definition: {
     openapi: '3.0.0',
     info: {
       title: 'Neptun Express Backend API',
-      version: '1.0.1',
+      version: '1.1.0',
       description: 'Qısaca API-nin təsviri',
     },
+    servers: [
+      {
+        url: 'https://neptunbk.vercel.app', // Vercel-də çalışan serverin URL-i
+      },
+    ],
     components: {
       securitySchemes: {
         bearerAuth: {
           type: 'http',
           scheme: 'bearer',
-          bearerFormat: 'JWT', // Obyektiv format (bu JWT üçün nümunədir)
+          bearerFormat: 'JWT',
         },
       },
     },
@@ -24,8 +56,9 @@ const options = {
       },
     ],
   },
-  apis: ['./src/routes/*.js'], // Path to your API routes
+  apis: ['./src/routes/*.js'],
 };
+
 
 const specs = swaggerJsdoc(options);
 
